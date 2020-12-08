6c86b83366a05662ec75793070fa51d9fcf4e770
Josh Piedimonte <joshpied@gmail.com>
2020-12-08 13:40:50 -0500

    More styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,23 @@ module ApplicationHelper
         "alert-danger"
     end
   end
+
+  def day_of_week(date)
+    day = date.wday
+    if day == 0
+      return "Sunday"
+    elsif day == 1
+      return "Monday"
+    elsif day == 2
+      return "Tuesday"
+    elsif day == 3
+      return "Wednesday"
+    elsif day == 4
+      return "Thursday"
+    elsif day == 5
+      return "Friday"
+    else
+      return "Saturday"
+    end
+  end
 end
